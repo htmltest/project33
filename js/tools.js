17797556fd5a944da3c76cfa6d64dba7defdc94a
HTML Test <struev.vs@gmail.com>
2014-12-27 05:35:15 +0300

    1.3

--- js/tools.js
+++ js/tools.js
@@ -42,7 +42,12 @@
                 $('.item-others-next .item-others-name').html($('.list-content-items tr').eq(nextIndex).find('.list-content-name-text-1').html());
                 $('.item-others-next .item-others-genre').html($('.list-content-items tr').eq(nextIndex).find('.list-content-name-inner').attr('rel'));
 
-                $('.item').load(curLink.attr('href'), function() {
+                $.ajax({
+                    url: curLink.attr('href'),
+                    dataType: 'html',
+                    cache: false
+                }).done(function(html) {
+                    $('.item').html(html);
                     $('.item-wrap .loading').hide();
                 });
             }
@@ -68,6 +73,68 @@
             }
             $('.list-content-items tr').eq(curIndex).find('.list-content-name-inner').click();
             e.preventDefault();
+        });
+
+        $('.messages-list-content a').click(function(e) {
+            var curLink = $(this);
+            if (!curLink.hasClass('active')) {
+                $('.messages-content .loading').show();
+                $('.messages-list-content a.active').removeClass('active');
+                curLink.addClass('active');
+                $.ajax({
+                    url: curLink.attr('href'),
+                    dataType: 'html',
+                    cache: false
+                }).done(function(html) {
+                    $('.messages-content-inner').html(html);
+                    $('.messages-content .loading').hide();
+                });
+            }
+            e.preventDefault();
+        });
+
+        $('.item-wrap form').each(function() {
+            $('#fileupload-foto').fileupload({
+                url: 'js/jquery.fileupload/server/php/',
+                acceptFileTypes: /(\.|\/)(jpg)$/i,
+                autoUpload: true,
+                start: function() {
+                    $('.item-wrap form').addClass('disabled');
+                },
+                stop: function() {
+                    $('.item-wrap form').removeClass('disabled');
+                }
+            });
+
+            $('#fileupload-audio').fileupload({
+                url: 'js/jquery.fileupload/server/php/',
+                acceptFileTypes: /(\.|\/)(mp3|wav)$/i,
+                autoUpload: true,
+                start: function() {
+                    $('.item-wrap form').addClass('disabled');
+                },
+                stop: function() {
+                    $('.item-wrap form').removeClass('disabled');
+                }
+            });
+
+            $('#fileupload-video').fileupload({
+                url: 'js/jquery.fileupload/server/php/',
+                acceptFileTypes: /(\.|\/)(mp4|avi)$/i,
+                autoUpload: true,
+                start: function() {
+                    $('.item-wrap form').addClass('disabled');
+                },
+                stop: function() {
+                    $('.item-wrap form').removeClass('disabled');
+                }
+            });
+
+            $('.item-input-link span').click(function() {
+                var curItem = $(this).parents().filter('.form-filelink-top');
+                curItem.find('.item-input').show();
+                curItem.find('.item-input-link').hide();
+            });
         });
 
     });
@@ -98,6 +165,12 @@
 
     $(window).load(function() {
         $('.list-content-items tr:first .list-content-name-inner').click();
+
+        $('.messages-list-container').jScrollPane({
+                autoReinitialise: true
+        });
+
+        $('.messages-list-content a:first').click();
     });
 
 })(jQuery);
